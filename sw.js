086606fe53426c6c16430d4a8d21ce624adf333d
mbgk0306-maker <mbgk0306@gmail.com>
2026-06-13 10:12:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// 深海ステーション物語 Service Worker:アプリシェルをキャッシュして完全オフライン対応
+const VER = 'shinkai-v1';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(VER).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==VER).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request, {ignoreSearch:true}).then(hit=>
+      hit || fetch(e.request).then(res=>{
+        if(res.ok && new URL(e.request.url).origin===location.origin){
+          const cp=res.clone(); caches.open(VER).then(c=>c.put(e.request, cp));
+        }
+        return res;
+      }).catch(()=> caches.match('./index.html'))
+    )
+  );
+});
